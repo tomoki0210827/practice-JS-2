2dc33c21002ca794244f68657e4513121454bad3
tomoki0210827 <73229071+tomoki0210827@users.noreply.github.com>
2020-10-23 14:29:04 +0900

    Add files via upload

---
+++ foo.js
@@ -0,0 +1,6 @@
+const { default: youtuber } = require(".")
+
+import youtuber from ",/foo";
+youtuber.plan()
+
+youtuber.list
---
+++ index.js
@@ -0,0 +1,22 @@
+let youtuber = {
+    list: {
+        business: {
+            youtuber: [
+                { name:"ともき", age: 21, teachProgramming() {} },
+                { name: "あっちゃん", age 30, teachHistroy() {} }
+            ],
+            teach() {}
+        },
+        entertainment: {
+            youtuber: [{ name: "ヒカキン"}, { name:"はじめしゃちょー"}],
+            makesmile() {}
+        }
+    },
+    plan() {}
+    uploadVideo() {}
+};
+
+window.console.log(); 
+
+window.alert();
+window.document.getElementById()
